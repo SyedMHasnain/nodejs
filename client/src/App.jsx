50ba8496a4eff1a8945cbe477f7c6db907a08b6b
Syed_hasnain <husnain486@gmail.com>
2024-08-24 04:26:48 +0500

    api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, {  } from 'react'
+import Header from './Components/Header';
 
 const App = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("/api") //fetching data from the server
-      .then((res) => res.json()) // display the data in json format
-      .then((data) => setData(data)); // set the data in the state
-  }, []);
+  
   return (
-    <>
-      
-      {
-        typeof data == "undefined" ? (
-        <p> Loading...</p>
-      )
-       : (
-        data.map((user, index) => 
-        (
-         <div> <h2 key={index}>{user.shop}</h2>
-         <h2 >{user.ceo}</h2>
-         </div>
-         ))
-      )}
-    </>
+    <div>
+<Header/>
+    </div>
   );
-};
+}
 
-export default App;
+export default App
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+const Header = () => {
+  const [Backenddata , setBackendData] = useState([{}]);
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setBackendData(data));
+  }, []);
+  return (
+    <>
+      <div className="bg-blue-500 text-white text-center text-2xl p-4">
+        APi DAta
+      </div>
+      <div>
+        {typeof Backenddata.users === "undefined" ? (
+          <h1>Loading.....</h1>
+        ) : (
+          Backenddata.users.map((user, index) => (
+            <div className="flex   justify-center">
+              <h4
+                className=" justify-center align-middle p-10 w-28 flex bg-lime-300 rounded-full"
+                key={index}>
+                {user}
+              </h4>
+            </div>
+          ))
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Header
